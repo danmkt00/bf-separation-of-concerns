c8412535c589376361c0aefc9a1bc36887c49082
danmkt00 <dani4ku@gmail.com>
2025-04-29 12:03:56 +0200

    add 5-rendering-data exercises

--- 5-rendering-data/exercises-from-scratch/1-styled-header/actual.js
+++ 5-rendering-data/exercises-from-scratch/1-styled-header/actual.js
@@ -1,3 +1,9 @@
 'use strict';
 
-const actual = (level, text, styling) => {};
+const actual = (level, text, styling) => {
+  const hEl = document.createElement(`h${level}`);
+  hEl.className = styling;
+  hEl.innerText = text;
+
+  return hEl;
+};
--- 5-rendering-data/exercises-from-scratch/2-description-list/actual.js
+++ 5-rendering-data/exercises-from-scratch/2-description-list/actual.js
@@ -4,19 +4,42 @@
 // used to convert object property names (camel case)
 // into titles (words with spaces between them)
 const camelCaseToTitleCase = (camelCaseString) => {
-    const result = camelCaseString
-        .replace(/([a-z])([A-Z][a-z])/g, '$1 $2')
-        .replace(/([A-Z][a-z])([A-Z])/g, '$1 $2')
-        .replace(/([a-z])([A-Z]+[a-z])/g, '$1 $2')
-        .replace(/([A-Z]+)([A-Z][a-z][a-z])/g, '$1 $2')
-        .replace(/([a-z]+)([A-Z0-9]+)/g, '$1 $2')
-        .replace(/([A-Z]+)([A-Z][a-rt-z][a-z]*)/g, '$1 $2')
-        .replace(/([0-9])([A-Z][a-z]+)/g, '$1 $2')
-        .replace(/([A-Z]{2,})([0-9]{2,})/g, '$1 $2')
-        .replace(/([0-9]{2,})([A-Z]{2,})/g, '$1 $2')
-        .trim();
+  const result = camelCaseString
+    .replace(/([a-z])([A-Z][a-z])/g, '$1 $2')
+    .replace(/([A-Z][a-z])([A-Z])/g, '$1 $2')
+    .replace(/([a-z])([A-Z]+[a-z])/g, '$1 $2')
+    .replace(/([A-Z]+)([A-Z][a-z][a-z])/g, '$1 $2')
+    .replace(/([a-z]+)([A-Z0-9]+)/g, '$1 $2')
+    .replace(/([A-Z]+)([A-Z][a-rt-z][a-z]*)/g, '$1 $2')
+    .replace(/([0-9])([A-Z][a-z]+)/g, '$1 $2')
+    .replace(/([A-Z]{2,})([0-9]{2,})/g, '$1 $2')
+    .replace(/([0-9]{2,})([A-Z]{2,})/g, '$1 $2')
+    .trim();
 
-    return result.charAt(0).toUpperCase() + result.slice(1);
+  return result.charAt(0).toUpperCase() + result.slice(1);
 };
 
-const actual = (obj) => {};
+const actual = (obj) => {
+  const dlEl = document.createElement('dl');
+
+  for (const key in obj) {
+    if (key.startsWith('_')) {
+      continue;
+    }
+
+    const dtEl = document.createElement('dt');
+    dtEl.innerText = camelCaseToTitleCase(key);
+
+    const ddEl = document.createElement('dd');
+    ddEl.className = 'value';
+
+    ddEl.innerText = String(obj[key]);
+
+    //ddEl.innerText = true;
+    // ddEl.innerText = null;
+
+    dlEl.append(dtEl, ddEl);
+  }
+
+  return dlEl;
+};
